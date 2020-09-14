9b69e8a028d17bbdcb121d43176c9bd51d0f7d4b
Łukasz Zapolski <lukasz.zapolski@booksy.com>
2020-09-14 20:57:57 +0200

    Add tweet model

---
+++ app/models/tweet.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose'
+
+import { UserDocument } from './user'
+
+const Schema = mongoose.Schema
+
+export type TweetDocument = mongoose.Document & {
+	author: UserDocument
+	body: string
+	favorites: UserDocument[]
+	comments: TweetComment[]
+}
+
+interface TweetComment {
+	body: string
+	author: UserDocument
+	favorites: UserDocument[]
+	createdAt: Date
+}
+
+const tweetSchema = new Schema(
+	{
+		body: { type: String, default: '', trim: true, maxlength: 280 },
+		author: { type: Schema.Types.ObjectId, ref: 'User' },
+		favorites: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+		comments: [
+			{
+				body: { type: String, default: '', maxlength: 280 },
+                author: { type: Schema.Types.ObjectId, ref: 'User' },
+                favorites: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+				createdAt: { type: Date, default: Date.now },
+			},
+		],
+	},
+	{ timestamps: true }
+)
+
+export const Tweet = mongoose.model<TweetDocument>('Tweer', tweetSchema)
